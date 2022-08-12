300551e05c66f3a5c2f90fa9e4fcef20d2b3b121
Rocky <25568640+rockindahizzy@users.noreply.github.com>
2022-08-12 07:49:55 -0500

    Initial Commit

---
+++ cypress.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "cypress";
+import * as path from "path";
+import {deleteSync} from 'del';
+
+export default defineConfig({
+  e2e: {
+    async setupNodeEvents(on, config) {
+      const version = config.env.version || 'development'
+      config.projectId = "5jgpns"
+      config.defaultCommandTimeout = 10000
+      config.chromeWebSecurity = false
+      // on("task", {
+      //   async checkGmail(args: CheckGmailParam) {
+      //     return await checkGmail(args);
+      //   },
+      // });
+      on('after:spec', (spec, results) => {
+        if (results && results.stats.failures === 0 && results.video) {
+          // `del()` returns a promise, so it's important to return it to ensure
+          // deleting the video is finished before moving on
+          deleteSync(results.video)
+        }
+      })
+      return config
+    },
+    reporter: 'mochawesome'
+  },
+});
